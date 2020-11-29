70f2ae411efa73cd882c89cb6ae1b85f43111b12
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-11-30 01:45:44 +1100

    Completed Read Task

--- MovieProgram/webApi/Controllers/ConController.cs
+++ MovieProgram/webApi/Controllers/ConController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
+using MovieClass;
+using System.Collections.Generic;
 
 namespace webApi.Controllers
 {
@@ -9,15 +11,21 @@ namespace webApi.Controllers
     public class ConController : ControllerBase
     { 
         public string ConString { get; set;}
+        public string ConnectionString { get; set;}
+        public List<Movie> Movies { get; set;} 
+        public List<string> MovieTitles { get; set;} 
 
         public ConController(){
             this.ConString = @"no.database.here.com;Initial Catalog=Is;User ID=Wally; Password=Where";
+            this.ConnectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=Movies;User ID=admin; Password=kereneritrea";
+            this.Movies = new List<Movie>();
+            this.MovieTitles = new List<string>();
         }
 
-        [HttpGet]
-        public string Get()
-        {
-           
+        [HttpGet("Exception")]
+        public string CatchException()
+        { 
           try{
                using (SqlConnection con = new SqlConnection(this.ConString))
                 {
@@ -34,5 +42,126 @@ namespace webApi.Controllers
            }
 
         }
+
+
+        [HttpGet("GetAllMovies")]
+        public List<Movie> GetAllMovies(){
+            string connectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=Movies;User ID=admin; Password=kereneritrea";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = "SELECT * FROM MOVIE";
+            SqlCommand command = new SqlCommand(queryString, con);
+
+            con.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Movies.Add(
+                        new Movie(Convert.ToInt32(reader[0]), reader[1].ToString(), Convert.ToInt32(reader[2]), Convert.ToInt32(reader[3]))
+                    );
+                }
+            }
+
+            return this.Movies;
+        }
+
+        [HttpGet("FindMovie")]
+        public List<string> FindMovie(){
+            string connectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=Movies;User ID=admin; Password=kereneritrea";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = "SELECT TITLE FROM MOVIE WHERE TITLE LIKE 'THE%';";
+            SqlCommand command = new SqlCommand(queryString, con);
+
+            con.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    MovieTitles.Add(
+                        reader[0].ToString()
+                    );
+                }
+            }
+
+            return this.MovieTitles;
+        }
+
+         [HttpGet("TitleForActorCasted")]
+        public List<string> TitleForActorCasted(){
+            string connectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=Movies;User ID=admin; Password=kereneritrea";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = @"SELECT TITLE FROM
+                                (
+                                SELECT C.ACTORNO, C.MOVIENO, M.TITLE
+                                FROM  ((CASTING C
+                                INNER JOIN ACTOR A 
+                                ON C.ACTORNO = A. ACTORNO)
+                                INNER JOIN MOVIE M 
+                                ON M.MOVIENO = C.MOVIENO)
+                                ) S
+                                INNER JOIN
+                                (
+                                SELECT ACTORNO FROM ACTOR WHERE FULLNAME = 'Luke Wilson'
+                                ) A
+                                ON S.ACTORNO = A.ACTORNO
+                                WHERE S.ACTORNO = A.ACTORNO;";
+            SqlCommand command = new SqlCommand(queryString, con);
+
+            con.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    MovieTitles.Add(
+                        reader[0].ToString()
+                    );
+                }
+            }
+
+            return this.MovieTitles;
+        }
+
+        [HttpGet("MoviesTotalRunTime")]
+        public int MoviesTotalRunTime(){
+            string connectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=Movies;User ID=admin; Password=kereneritrea";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = "SELECT * FROM MOVIE";
+            SqlCommand command = new SqlCommand(queryString, con);
+
+            con.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Movies.Add(
+                        new Movie(Convert.ToInt32(reader[0]), reader[1].ToString(), Convert.ToInt32(reader[2]), Convert.ToInt32(reader[3]))
+                    );
+                }
+            }
+
+            var total = 0;
+            foreach(Movie m in this.Movies){
+                total += m.RunTime;
+            }
+            
+            return total;
+        }
+
     }
 }
